af6c3cf67dbd3b7399158492a142d2755cf74bd0
Brian Chang <changbhc@gmail.com>
2019-07-05 00:38:35 +0000

    Completed 2 tests in breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,17 @@
 const request = require("request");
 
 const fetchBreedDescription = function(breedName, cb) {
-  request(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedName}`, (error, response, body) => {
-    const data = JSON.parse(body);
-    cb(error, data);
+  request(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedName.substr(0,4)}`, (error, response, body) => {
+    let data = JSON.parse(body);
+    // console.log(`data is`, data);
+    // console.log(`data[0] is`, data[0]);
+    let desc;
+    if (!data[0]) {
+      desc = `Cannot find ${breedName}.\nPlease enter another breed!`;
+    } else {
+      desc = data[0].breeds[0].description.trim();
+    }
+    cb(error, desc);
   });
 };
 
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const { fetchBreedDescription } = require("./breedFetcher");
 const input = process.argv[2];
-const breedName = input.substr(0, 4).toLowerCase();
 
-fetchBreedDescription(breedName, (error, data) => {
-  if (!data[0]) { 
+fetchBreedDescription(input, (error, desc) => {
+  if (!desc) {
     console.log(`Cannot find ${input}.\nPlease enter another breed!`);
-    process.exit();
   }
   if (error) {
     console.log(`Unable to access API.\nERROR: ${error}`);
   } else {
-    console.log(data[0].breeds[0].description);
+    console.log(desc);
   }
 });
